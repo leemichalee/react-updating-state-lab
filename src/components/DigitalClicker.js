4180f145f85f45698feea455244869f542e46118
leemichalee <leeisr@gmail.com>
2020-12-10 01:47:23 -0500

    commit tree 8a34af1c0ea7651e9bd9f4c9bba00ffff7dac0f9

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,23 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+
+class DigitalClicker extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            timesClicked: 0
+        }
+    }
+    buttonHandler = () => {
+        let clickerUpper = this.state.timesClicked + 1
+        this.setState({
+            timesClicked: clickerUpper
+        })
+    }
+    render() {
+        return (
+            <button onClick = {this.buttonHandler}>{this.state.timesClicked}</button>
+        )
+    }
+}
+export default DigitalClicker
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,33 @@
 // Code YouTubeDebugger Component Here
+import React from 'react'
+
+class YouTubeDebugger extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+              bitrate: 8,
+              video: {
+                resolution: '1080p'
+            }
+        }
+      } 
+    }
+    bitrateHandler = () => {
+        this.state.settings.bitrate = 12
+    }
+    resolutionHandler = () => {
+        this.state.settings.video.resolution = '720p'
+    }
+    render() {
+        return (
+            <div>
+                <button className='bitrate' onClick={this.bitrateHandler}></button>
+                <button className='resolution' onClick={this.resolutionHandler}></button>
+            </div>
+        )
+    }
+}
+export default YouTubeDebugger
